83dbd23fdf94cd0264209d024b4e501b2efcda2b
Marcnabil10 <marcnabil2004@gmail.com>
2025-11-20 17:13:27 +0200

    DataBase

---
+++ src/BackEnd/DataBase.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BackEnd;
+
+import java.util.List;
+
+/**
+ *
+ * @author marcn
+ */
+public interface DataBase {
+    void save(User user);
+    void save(Course course);
+    boolean update(User user);
+    boolean update(Course course);
+    boolean deleteCourse(String courseId);
+    User getUserById(String id);
+    Course getCourseById(String id);
+    List<User> getAllUsers();
+    List<Course> getAllCourses();
+    User findUserByUsername(String username);
+    User findUserByEmail(String email);
+     
+}
